dae6177cf45938655261dcd3b8ff8372511c8f69
Alok Negi <aloknegi@Aloks-MacBook-Air.local>
2025-11-21 21:56:46 +0530

    feat: header completed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,30 @@
+import { ArrowLeft } from "lucide-react";
 import { Heading } from "../Typography";
 import "./Header.css";
+import { useLocation, useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const isBackPage =
+    pathname === "/add-task" || pathname.startsWith("/edit-task");
+
+  const getHeaderTitle = () => {
+    if (pathname === "/add-task") return "Add Task";
+    if (pathname.startsWith("/edit-task")) return "Edit Task";
+    return "TO DO APP";
+  };
+
   return (
     <div className="header-container">
-      <Heading color="white">TO DO APP</Heading>
+      {isBackPage && (
+        <button className="back-btn" onClick={() => navigate(-1)}>
+          <ArrowLeft />
+        </button>
+      )}
+
+      <Heading color="white">{getHeaderTitle()}</Heading>
     </div>
   );
 };
